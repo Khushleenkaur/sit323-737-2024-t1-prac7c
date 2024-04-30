1a183601dd9eb6f6f671ee84795f70431e697ed9
Khushleen kaur <110883711+Khushleenkaur@users.noreply.github.com>
2024-04-30 18:35:34 +0530

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,1 +1,9 @@
+// Define a simple function to return a greeting message
+function getGreeting() {
+    return "Hello World!";
+}
 
+// Export the function for use in other files
+module.exports = {
+    getGreeting
+};
